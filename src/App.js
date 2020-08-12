b7b7c29d46fcdf1558861c628397cb73648d93d8
Nick Mardon <nickmardondigital@gmail.com>
2020-08-12 09:50:30 -0700

    added search input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Search/>
     </div>
   );
 }
